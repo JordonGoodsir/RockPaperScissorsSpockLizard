d02788d0bb713e1c6f15edecb14f806ff093e193
Jordon Goodsir <Jordon.goodsir1@gmail.com>
2024-04-12 11:12:10 +1000

    changed options selection rendering

--- src/components/PlayOptionsSelection.jsx
+++ src/components/PlayOptionsSelection.jsx
@@ -1,14 +1,22 @@
 import PlayOption from "./PlayOption"
 
+const optionStyles = {
+  scissors: 'absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2',
+  paper: 'absolute top-[20%] right-0 translate-x-1/2',
+  rock: 'absolute bottom-0 right-0 translate-y-1/2',
+  spock: 'absolute top-[20%] left-0 -translate-x-1/2',
+  lizard: 'absolute bottom-0 left-0 translate-y-1/2'
+}
+
 function PlayOptionsSelection() {
   return (
     <div className="flex flex-col relative w-[200px] lg:w-[350px]">
       <img src="../../public/images/pentagon.svg" />
-      <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2"><PlayOption option="scissors" /></div>
-      <div className="absolute top-[20%] right-0 translate-x-1/2"><PlayOption option="paper" /></div>
-      <div className="absolute top-[20%] left-0 -translate-x-1/2"><PlayOption option="spock" /></div>
-      <div className="absolute bottom-0 left-0 translate-y-1/2"><PlayOption option="lizard" /></div>
-      <div className="absolute bottom-0 right-0 translate-y-1/2"><PlayOption option="rock" /></div>
+      {Object.keys(optionStyles).map((option) => {
+        return (
+          <div className={optionStyles[option]}><PlayOption option={option} /></div>
+        )
+      })}
     </div>
   )
 }
